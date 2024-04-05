69a7bb024e1bef986e63dec19034a3bb94f12f3e
Laurin-Notemann <laurin.notemann@gmail.com>
2024-04-05 22:07:02 +0200

    chore: update dockfiles to use pnpm for workspaces

--- backend/scripts/prod-run-kube-migration.sh
+++
@@ -1,2 +0,0 @@
-kubectl cp ~/work/rememberry/rememberry/backend/drizzle rememberry/rem-db-stateset-0:/tmp
-cat ~/work/rememberry/rememberry/backend/scripts/kube-execute-sql.sh | kubectl exec -i -n rememberry rem-db-stateset-0 -- sh
